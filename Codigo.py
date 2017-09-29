fc2508c264194ded2e94596d2f6194fe870044fb
Tulmot <etc0007@alu.ubu.es>
2017-09-29 17:15:11 +0200

    Add files via upload

--- Codigo.py
+++ Codigo.py
@@ -4,24 +4,34 @@ from sklearn.datasets import make_multilabel_classification
 from scipy.spatial import distance
 from random import randint
 from random import shuffle
-class DisturbingNeigbors:
+class DisturbingNeigbors():
+    #Inicializamos variables
+    def __init__(self,
+                 n_instancias=10,
+                 n_caracteristicas=8,
+                 n_clases=5):
+        super(DisturbingNeigbors, self).__init__(
+            n_instancias,
+            n_caracteristicas,
+            n_clases)
+    
    #Calculamos el conjunto de datos
-    def conjunto_datos(n_instancias, n_caracteristicas,n_clases):
-        X, Y = make_multilabel_classification(n_samples=n_instancias, 
-                                              n_features=n_caracteristicas, n_classes=n_clases)
+    def conjunto_datos(self):
+        X, Y = make_multilabel_classification(n_samples=self.n_instancias, 
+                                              n_features=self.n_caracteristicas, n_classes=self.n_clases)
         return X, Y
     
     #Calculamos un array aleatorio boolean que es el que nos indicara que caracteristicas
     #valoraremos a la hora de calcular la distancia al vecino mas cercano
-    def aleatorio_Boolean(n_caracteristicas):
-        RndDimensions=np.random.randint(0, 2,n_caracteristicas)
+    def aleatorio_Boolean(self):
+        RndDimensions=np.random.randint(0, 2,self.n_caracteristicas)
         RndDimensions=RndDimensions.astype(bool)
         return RndDimensions
      
     #Calculamos un array aleatorio para seleccionar unas instancias aleatorias que
     #seran con las que calculemos la distancia al vecino mas cercano    
-    def aleatorio_Array(n_instancias):
-        s=list(range(n_instancias))
+    def aleatorio_Array(self):
+        s=list(range(self.n_instancias))
         random.shuffle(s)
         RndNeighbors=np.array(s[:(int(len(s)/2))])
         return RndNeighbors
