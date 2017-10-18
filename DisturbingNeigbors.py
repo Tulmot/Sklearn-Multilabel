c5fa6ef7d586c24e6b08a4da4779f13a5d71c3d3
Tulmot <etc0007@alu.ubu.es>
2017-10-18 19:02:18 +0200

    Complementado un método
    También corregido algún error, y empezado a comentar

--- DisturbingNeigbors.py
+++ DisturbingNeigbors.py
@@ -1,3 +1,7 @@
+"""Bagging meta-estimator."""
+
+# Author: Eduardo Tubilleja Calvo
+
 import numpy as np
 from random import shuffle
 #from sklearn.datasets import make_multilabel_classification
@@ -8,20 +12,50 @@ from sklearn.tree import DecisionTreeClassifier
 import math
 
 class DisturbingNeigbors:
+    """A Disturbing Neigbors.
+     Parameters
+    ----------
+    base_estimator : Es el clasificador que usaremos para entrenar nuestro
+        conjunto de datos, lo que recibe es o vacio o un objeto, si es vacio
+        por defecto se usa el DecisionTreeClassifier.
+        
+    n_neighbors : Son los vecinos molestones que queremos elegir del
+        conjunto de datos, por defecto si no se le pasa nada se eligen 10.
+        
+    n_features : Es el tamanno del sub-espacio aleatorio, según el cual
+        se eligen las caracteristicas al azar que vamos a usar para entrenar
+        nuestro clasificador, por defecto es 0.5, es decir se coge la mitad 
+        de las caracteristicas, si el valor que se le pasa es mayor de 1,
+        se coge esa cantidad de caracteristicas.
+        
+    rnd_dimensions : Un array aleatorio booleano, su tamanno es igual al 
+        numero de caracteristicas del conjunto, aunque luego el numero de
+        valores TRUE que contendra seran iguales a el valor de la variable
+        n_features, los valores TRUE, indican que caracteristicas son las
+        elegidas para valorar el conjunto.
+        
+    rnd_neighbors : Un array aleatorio de enteros,el tamanno de este array 
+        depende de la variable n_neighbors, seleccionara filas aleatorias 
+        del conjunto de datos, es lo que llamaremos vecinos molestones.
+        
+    """
     #Inicializamos variables
     def __init__(self,
                  base_estimator=DecisionTreeClassifier,
-                 n_vecinos=10,
+                 n_neighbors=10,
                  n_features=0.5):
         self.base_estimator =base_estimator 
-        self.n_vecinos=n_vecinos
+        self.n_neighbors=n_neighbors
         self.n_features=n_features
         self.rnd_dimensions
         self.rnd_neighbors
         
     #Calculamos el numero de caracteristicas que usaremos
     def _calculate_features(self,X):
-        return X.shape[1]*self.n_features
+        if  self.n_features<1:
+            return X.shape[1]*self.n_features
+        else:
+            return self.n_features
     
     #Calculamos un array random boolean que es el que nos indicara que 
     #caracteristicas que valoraremos
@@ -34,7 +68,7 @@ class DisturbingNeigbors:
         tam=X.shape[0]
         s=list(range(tam))
         shuffle(s)
-        return np.array(s[:(self.n_vecinos)])
+        return np.array(s[:(self.n_neighbors)])
     
     #Reducimos los datos obtenidos a las caracteristicas que vamos a evaluar,
     #que seran las que hemos obtenido segun el array random boolean
@@ -44,7 +78,7 @@ class DisturbingNeigbors:
     #Calculamos los vecinos mas cercanos a las instancias escogidas antes
     #aleatoriamente
     def _nearest_neighbor(self,m_reducida):
-        m_vecinos=np.zeros((self.n_vecinos,len(self.rnd_neighbors)))
+        m_neighbors=np.zeros((self.n_neighbors,len(self.rnd_neighbors)))
         cont=-1
         for i in m_reducida:
             dist=math.inf
@@ -57,8 +91,8 @@ class DisturbingNeigbors:
                     dist=dist2
                     a=cont
                     b=cont2
-            m_vecinos[a][b]=1
-        return m_vecinos
+            m_neighbors[a][b]=1
+        return m_neighbors
     
     #Funcion que llama a los metodos necesarios para devolver el fit
     def fit(self,X,Y):
@@ -66,16 +100,16 @@ class DisturbingNeigbors:
         self.rnd_dimensions=self._random_boolean(self)
         self.rnd_neighbors=self._random_array(self,X)
         m_reducida=self._reduce_data(self,X)
-        m_vecinos=self._nearest_neighbor(self,m_reducida)
-        m_entrenamiento=np.concatenate((X,m_vecinos),axis=1)
+        m_neighbors=self._nearest_neighbor(self,m_reducida)
+        m_entrenamiento=np.concatenate((X,m_neighbors),axis=1)
         return self.base_estimator.fit(m_entrenamiento,Y)
     
     #Recibe la otra parte del conjunto de datos, a partir de la cual vamos
     #a poder predecir luego
     def predict(self,X1):
         m_reducida2=self._reduce_data(self,X1)
-        m_vecinos2=self._nearest_neighbor(self,m_reducida2)
-        m_entrenamiento2=np.concatenate((X1,m_vecinos2),axis=1)
+        m_neighbors2=self._nearest_neighbor(self,m_reducida2)
+        m_entrenamiento2=np.concatenate((X1,m_neighbors2),axis=1)
         return self.base_estimator.predict(m_entrenamiento2)
     
     
