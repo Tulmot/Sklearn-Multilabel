4bc36c966662a4276950f1fb447fef36a1b05636
Tulmot <etc0007@alu.ubu.es>
2017-10-31 18:24:17 +0100

    Probamos que el algoritmo funciona correctamente

--- sklearn-ubu/Prueba_clase.py
+++ sklearn-ubu/Prueba_clase.py
@@ -1,12 +1,12 @@
 from disturbing_neighbors import DisturbingNeighbors
 from sklearn.datasets import make_multilabel_classification
+from sklearn.metrics import hamming_loss
 import numpy as np
 
 
 seed = 0
 
 X,Y=make_multilabel_classification(n_samples=32, n_features=10, random_state=seed) 
-print(X,Y)
 dn=DisturbingNeighbors(random_state=seed)
 
 matrizFitX = np.matrix([X[i,:] for i in range(16)], dtype=int)
@@ -16,5 +16,12 @@ dn.fit(matrizFitX,matrizFitY)
 
 
 matrizPredict=np.matrix([X[j+16,:] for j in range(len(X)-16)], dtype=int)
+y_true=np.matrix([Y[j+16,:] for j in range(len(Y)-16)], dtype=int)
 
-dn.predict(matrizPredict)
+y_predict=dn.predict(matrizPredict)
+
+
+dist=hamming_loss(y_true, y_predict)
+
+print(dist)
+
