4fbfa761ef40f1f79d27e95434e46f6e5343c2d2
Tulmot <etc0007@alu.ubu.es>
2017-11-14 17:31:19 +0100

    Add files via upload
    Funcional

--- src/sklearn_ubu/disturbing_neighbors.py
+++ src/sklearn_ubu/disturbing_neighbors.py
@@ -5,11 +5,12 @@
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.tree import DecisionTreeClassifier
-import math
 from sklearn.utils import check_random_state
+from sklearn.base import ClassifierMixin
+from sklearn.base import BaseEstimator
 
 
-class DisturbingNeighbors:
+class DisturbingNeighbors(BaseEstimator,ClassifierMixin):
     """A Disturbing Neighbors.
 
      Parameters
@@ -48,7 +49,7 @@ class DisturbingNeighbors:
         self.random_state = random_state
         self._rnd_dimensions = None
         self._rnd_neighbors = None
-
+        
     def _calculate_features(self, X):
         """Calculamos el numero de caracteristicas que usaremos"""
         if self.n_features < 1:
@@ -74,26 +75,25 @@ class DisturbingNeighbors:
         boolean"""
         return X[:, self._rnd_dimensions]
 
-    def _nearest_neighbor(self, m_reduce):
+    def _disturbing(self,_m_reduce):
+        """Calculamos la matriz de los vecinos molestones."""
+        print(_m_reduce)
+        print(self._rnd_neighbors)
+        m_disturbing=_m_reduce[self._rnd_neighbors,:]
+        return m_disturbing
+
+    def _nearest_neighbor(self,_m_reduce):
         """Calculamos los vecinos mas cercanos a las instancias escogidas
         antes aleatoriamente"""
-        m_neighbors = np.zeros((len(m_reduce), len(self._rnd_neighbors)))
-        indice_ins = -1
-        for instancia in m_reduce:
-            dist = math.inf
-            indice_ins += 1
-            indice_vec = -1
-            for j in self._rnd_neighbors:
-                indice_vec += 1
-                dist_tmp = euclidean_distances(instancia, m_reduce[j, :])
-                if dist_tmp < dist:
-                    dist = dist_tmp
-                    a = indice_ins
-                    b = indice_vec
-            m_neighbors[a][b] = 1
+        m_disturbing=self._disturbing(_m_reduce)
+        euc_dis_func = lambda t: euclidean_distances([t],m_disturbing).argmin()
+        dn_nn = lambda inst : np.apply_along_axis(euc_dis_func,1,[inst])
+        m_nearest=np.apply_along_axis(dn_nn,1,_m_reduce)
+        neighbors = lambda vec : np.concatenate((np.concatenate((np.zeros(vec), [1]), axis=0), np.zeros(len(m_disturbing)-vec-1)), axis=0)
+        m_neighbors= np.apply_along_axis(neighbors,1,m_nearest)
         return m_neighbors
 
-    def fit(self, X, Y):
+    def fit(self, X, y):
         """Build a Bagging ensemble of estimators from the training set (X, y).
 
         Parameters
@@ -115,10 +115,10 @@ class DisturbingNeighbors:
         self.n_features = self._calculate_features(X)
         self._rnd_dimensions = self._random_boolean()
         self._rnd_neighbors = self._random_array(X)
-        m_reduce = self._reduce_data(X)
-        m_neighbors = self._nearest_neighbor(m_reduce)
+        _m_reduce = self._reduce_data(X)
+        m_neighbors = self._nearest_neighbor(_m_reduce)
         m_train = np.concatenate((X, m_neighbors), axis=1)
-        return self.base_estimator.fit(m_train, Y)
+        return self.base_estimator.fit(m_train, y)
 
     def predict(self, X):
         """Predict class for X.
@@ -136,8 +136,8 @@ class DisturbingNeighbors:
         y : It's a matrix of form = [n_class]
             The predicted classes.
         """
-        m_reduce = self._reduce_data(X)
-        m_neighbors = self._nearest_neighbor(m_reduce)
+        x_reduce = self._reduce_data(X)
+        m_neighbors = self._nearest_neighbor(x_reduce)
         m_train = np.concatenate((X, m_neighbors), axis=1)
         return self.base_estimator.predict(m_train)
 
@@ -158,7 +158,28 @@ class DisturbingNeighbors:
             The class probabilities of the input samples. The order of the
             classes corresponds to that in the attribute `classes_`.
         """
-        m_reduce = self._reduce_data(X)
-        m_neighbors = self._nearest_neighbor(m_reduce)
+        x_reduce2= self._reduce_data(X)
+        m_neighbors = self._nearest_neighbor(x_reduce2)
         m_train = np.concatenate((X, m_neighbors), axis=1)
         return self.base_estimator.predict_proba(m_train)
+    
+    def score(self, X, y, sample_weight=None):
+        """Returns the mean accuracy on the given test data and labels.
+        In multi-label classification, this is the subset accuracy
+        which is a harsh metric since you require for each sample that
+        each label set be correctly predicted.
+        Parameters
+        ----------
+        X : array-like, shape = (n_samples, n_features)
+            Test samples.
+        y : array-like, shape = (n_samples) or (n_samples, n_outputs)
+            True labels for X.
+        sample_weight : array-like, shape = [n_samples], optional
+            Sample weights.
+        Returns
+        -------
+        score : float
+            Mean accuracy of self.predict(X) wrt. y.
+        """
+        from sklearn.metrics import accuracy_score
+        return accuracy_score(y, self.predict(X), sample_weight=sample_weight)
