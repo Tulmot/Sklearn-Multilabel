5fad80d3c5f7fb23bfe2b22abaab6608036e1032
Tulmot <etc0007@alu.ubu.es>
2017-11-14 18:12:34 +0100

    Add files via upload

--- src/sklearn_ubu/Prueba_clase.py
+++ src/sklearn_ubu/Prueba_clase.py
@@ -2,38 +2,38 @@ from disturbing_neighbors import DisturbingNeighbors
 from sklearn.datasets import make_multilabel_classification
 from sklearn.metrics import hamming_loss
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble.bagging import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.multiclass import OneVsRestClassifier
+#from sklearn.neural_network import MLPClassifier
 import numpy as np
-from sklearn import tree
-import graphviz
+from sklearn.model_selection import cross_val_score
 
 seed = 0
 
 X,y=make_multilabel_classification(n_samples=32, n_features=10, random_state=seed) 
-dn=DisturbingNeighbors(random_state=seed)
+bc=OneVsRestClassifier(BaggingClassifier())
+#dn=BaggingClassifier(base_estimator=DisturbingNeighbors(random_state=seed))
+dn=DisturbingNeighbors(base_estimator=DecisionTreeClassifier(max_depth=3),random_state=seed)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, train_size=0.5, 
                                                     random_state=seed)
 
-X_train=np.matrix(np.array(X_train))
-X_test=np.matrix(np.array(X_test))
-y_train=np.matrix(np.array(y_train))
-y_test=np.matrix(np.array(y_test))
 
-arbol=dn.fit(X_train,y_train)
 
+
+train=dn.fit(X_train,y_train)
+#train2=bc.fit(X_train,y_train)
 y_predict=dn.predict(X_test)
-print(y_predict)
 y_predict_proba=dn.predict_proba(X_test)
-print(y_predict_proba)
 
 dist=hamming_loss(y_test, y_predict)
 
+scores = cross_val_score(dn, X, y)
+print(scores)
+
 print(dist)
 
-tree.export_graphviz(arbol, out_file='tree.dot') 
-
-dot_data = tree.export_graphviz(arbol, out_file=None) 
-graph = graphviz.Source(dot_data) 
 
 
 
