d5b6cb2a14ed85e9305f647491620ffd0a866577
Tulmot <etc0007@alu.ubu.es>
2017-11-16 17:31:58 +0100

    cross validation

--- src/sklearn_ubu/Prueba_clase.py
+++ src/sklearn_ubu/Prueba_clase.py
@@ -6,12 +6,11 @@ from sklearn.ensemble.bagging import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.multiclass import OneVsRestClassifier
 #from sklearn.neural_network import MLPClassifier
-import numpy as np
 from sklearn.model_selection import cross_val_score
 
 seed = 0
 
-X,y=make_multilabel_classification(n_samples=32, n_features=10, random_state=seed) 
+X,y=make_multilabel_classification(n_samples=100, n_features=10, random_state=seed) 
 bc=OneVsRestClassifier(BaggingClassifier())
 #dn=BaggingClassifier(base_estimator=DisturbingNeighbors(random_state=seed))
 dn=DisturbingNeighbors(base_estimator=DecisionTreeClassifier(max_depth=3),random_state=seed)
@@ -22,17 +21,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, train_s
 
 
 
-train=dn.fit(X_train,y_train)
+clas_train=dn.fit(X_train,y_train)
+
 #train2=bc.fit(X_train,y_train)
 y_predict=dn.predict(X_test)
 y_predict_proba=dn.predict_proba(X_test)
 
 dist=hamming_loss(y_test, y_predict)
 
+print(dist)
+
 scores = cross_val_score(dn, X, y)
 print(scores)
-
-print(dist)
 
 
 
