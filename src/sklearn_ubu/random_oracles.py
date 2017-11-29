7efbbd931c991ef78685fe9e0dedfa56573bad65
Tulmot <etc0007@alu.ubu.es>
2017-11-29 19:17:40 +0100

    cambios en el fit

--- src/sklearn_ubu/random_oracles.py
+++ src/sklearn_ubu/random_oracles.py
@@ -17,7 +17,7 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
 
     def __init__(self,
                  base_estimator=DecisionTreeClassifier(),
-                 n_oracles=10,
+                 n_oracles=3,
                  random_state=None):
         self.base_estimator = base_estimator
         self.n_oracles = n_oracles
@@ -49,7 +49,7 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         m_oracles = np.apply_along_axis(oracles, 1, m_nearest)
         return m_oracles
     
-    def fit(self, X):
+    def fit(self, X,y):
         """Build a Bagging ensemble of estimators from the training set (X, y).
         Parameters
         ----------
@@ -64,9 +64,38 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         self : object
             Returns self.
         """
+        def createMatrix(m_ora):
+            if m_ora>0:
+                a.append(X[self._index,:])
+                b.append(y[self._index,:])
+            self._index+=1
         self.random_state = check_random_state(self.random_state)
         self._rnd_oracles = self._calc_rnd_oracles(X)
         self._m_oracles = self._oracles(X)
         m_oracle = self._nearest_oracle(X)
-        m_train = np.concatenate((X, m_oracle), axis=1)
-        return self.base_estimator.fit(m_train)
+        for i in range(m_oracle.shape[1]):
+            a=[]
+            b=[]
+            self._index=0
+            list(map(createMatrix, m_oracle[:,i]))
+            arr1=np.asarray(a)
+            arr2=np.asarray(b)
+            print(self.base_estimator.fit(arr1,arr2))
+        #filter(lambda x: x[0, 2] > 0, data)
+        return self.base_estimator.fit(m_oracle,y)
+    
+    def predict(self, X):
+        """Predict class for X.
+        The predicted class of an input sample is computed as the class with
+        the highest mean predicted probability.
+        Parameters
+        ----------
+        X : It's a matrix of form = [n_instances, n_features]
+            The training input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+        Returns
+        -------
+        y : It's a matrix of form = [n_class]
+            The predicted classes.
+        """
+        return
