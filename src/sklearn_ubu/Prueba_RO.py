d038b565b94aca5ba5097cdb34fa452a408a4446
Tulmot <etc0007@alu.ubu.es>
2017-12-05 18:43:00 +0100

    Métodos fit, predict y predict_proba

--- src/sklearn_ubu/Prueba_RO.py
+++ src/sklearn_ubu/Prueba_RO.py
@@ -5,6 +5,7 @@ Created on Sat Nov 25 10:41:02 2017
 @author: Tubi
 """
 from random_oracles import BaseRandomOracles
+from disturbing_neighbors import DisturbingNeighbors
 from sklearn.datasets import make_multilabel_classification
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
@@ -13,8 +14,14 @@ seed = 0
 
 X, y = make_multilabel_classification(
         n_samples=50, n_features=10, random_state=seed)
+
 ro = BaseRandomOracles(base_estimator=DecisionTreeClassifier(
                        random_state=seed),random_state=seed)
+
+#ro = DisturbingNeighbors(
+#    random_state=seed, base_estimator_=BaseRandomOracles(
+#            random_state=seed, base_estimator=DecisionTreeClassifier(
+#                random_state=seed)))
 
 X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.5, train_size=0.5, random_state=seed)
@@ -22,3 +29,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 ro.fit(X_train,y_train)
 
 y_predict = ro.predict(X_test)
+print(y_predict)
+
+y_predict_proba = ro.predict_proba(X_test)
+print(y_predict_proba)
--- src/sklearn_ubu/random_oracles.py
+++ src/sklearn_ubu/random_oracles.py
@@ -63,24 +63,18 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         self : object
             Returns self.
         """
-        def createMatrix(num):
-            if num>0:
-                a.append(X[self._index,:])
-                b.append(y[self._index,:])
-            self._index+=1
+        
+        def train (my_list):
+            a=np.asarray(my_list).astype(bool)
+            Xp=X[a,:]
+            yp=y[a,:]
+            self._classifiers_train.append(self.base_estimator.fit(Xp,yp))
         self.random_state = check_random_state(self.random_state)
         self._rnd_oracles = self._calc_rnd_oracles(X)
         self._m_oracles = self._oracles(X)
         m_oracle = self._nearest_oracle(X)
-        classifiers_train=[]
-        for i in range(m_oracle.shape[1]):
-            a=[]
-            b=[]
-            self._index=0
-            list(map(createMatrix, m_oracle[:,i]))
-            arr1=np.asarray(a)
-            arr2=np.asarray(b)
-            classifiers_train.append(self.base_estimator.fit(arr1,arr2))
+        self._classifiers_train=[]
+        list(map(train,m_oracle.T))
             
     def predict(self, X):
         """Predict class for X.
@@ -96,6 +90,45 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         y : It's a matrix of form = [n_class]
             The predicted classes.
         """
+        def test(my_list):
+            a=np.asarray(my_list).astype(bool)
+            Xp=X[a,:]
+            self._index2+=1
+            self._classifiers_test.append(
+                    self._classifiers_train[self._index2].predict(Xp))
+           
+            
         m_oracle = self._nearest_oracle(X)
-        print(m_oracle)
-        return
+        self._classifiers_test=[]
+        self._index2=-1
+        list(map(test,m_oracle.T))            
+        return self._classifiers_test
+    
+    def predict_proba(self, X):
+        """The predicted class probabilities of an input sample is computed as
+        the mean predicted class probabilities of the base estimators in the
+        ensemble.
+         Parameters
+        ----------
+        X : It's a matrix of form = [n_instances, n_features]
+            The training input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+        Returns
+        -------
+        p : It's a matrix of form = [n_samples, n_classes]
+            The class probabilities of the input samples. The order of the
+            classes corresponds to that in the attribute `classes_`.
+        """
+        def test(my_list):
+            a=np.asarray(my_list).astype(bool)
+            Xp=X[a,:]
+            self._index2+=1
+            self._classifiers_test_proba.append(
+                    self._classifiers_train[self._index2].predict_proba(Xp))
+           
+            
+        m_oracle = self._nearest_oracle(X)
+        self._classifiers_test_proba=[]
+        self._index2=-1
+        list(map(test,m_oracle.T))            
+        return self._classifiers_test_proba
