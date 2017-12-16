74157a990713c872594af0b29943509a089bb923
Tulmot <etc0007@alu.ubu.es>
2017-12-16 13:29:25 +0100

    Comentado y quitamos código repetido

--- src/sklearn_ubu/base_random_oracles.py
+++ src/sklearn_ubu/base_random_oracles.py
@@ -7,7 +7,34 @@ from sklearn.base import BaseEstimator
 
 
 class BaseRandomOracles(ClassifierMixin, BaseEstimator):
+    """A Random Oracles.
+    
+    Random Oracles is a multi-label ensemble, each classifier in the set is
+    replaced by a miniensemble of a pair of subclassifiers with an oracle to
+    choose between them.
+    
+     Parameters
+    ----------
+    base_estimator_ : It is the classifier that we will use to train our data
+        set, what it receives is either empty or an object, if it is empty by
+        default the DecisionTreeClassifier is used.
+        
+    n_oracles : They are the oracles that we want to choose from the data
+        set, by default if nothing happens, 3 are chosen.
+        
+    random_state : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
 
+    _rnd_oracles : A random array of integers, the size of this array depends
+        on the variable n_oracles, will select random rows of the data set,
+        is what we will call random oracles.
+        
+    _m_oracles : It is a matrix that contains the instances of the random
+    sentences that we have selected.
+    """
     def __init__(self,
                  base_estimator=DecisionTreeClassifier(),
                  n_oracles=3,
@@ -26,8 +53,7 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
 
     def _oracles(self, X):
         """Calculamos la matriz de los oracles."""
-        self._m_oracles = X[self._rnd_oracles, :]
-        return self._m_oracles
+        return X[self._rnd_oracles, :]
 
     def _nearest_oracle(self, _m_reduce):
         """Calculamos los vecinos mas cercanos a las instancias escogidas
@@ -39,8 +65,7 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         oracles = lambda vec: np.concatenate((np.concatenate((
                                 np.zeros(vec), [1]), axis=0), np.zeros(len(
                                     self._m_oracles)-vec-1)), axis=0)
-        m_oracles = np.apply_along_axis(oracles, 1, m_nearest)
-        return m_oracles
+        return np.apply_along_axis(oracles, 1, m_nearest)
 
     def fit(self, X, y):
         """Build a Bagging ensemble of estimators from the training set (X, y).
@@ -67,9 +92,17 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         self.random_state = check_random_state(self.random_state)
         self._rnd_oracles = self._calc_rnd_oracles(X)
         self._m_oracles = self._oracles(X)
-        m_oracle = self._nearest_oracle(X)
-        self._classifiers_train = list(map(train, m_oracle.T))
+        self._classifiers_train = list(map(train, self._nearest_oracle(X).T))
 
+    def _split_inst_oracles(self, X, inst_oracles):
+        """Separamos la instanciay el oraculo, y de este ultimo obtenemos cual
+        es el oraculo más cercano a esa instancia """
+        n_features = X.shape[1]
+        oracle_near = inst_oracles[n_features:]
+        instance = inst_oracles[:n_features]
+        oracle_near = list(oracle_near)
+        index_classifier = oracle_near.index(1)
+        return instance, index_classifier
     def predict(self, X):
         """Predict class for X.
         The predicted class of an input sample is computed as the class with
@@ -87,14 +120,10 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         def list_predict(inst_oracles):
             """Predecimos cada una de las instancias con el oraculo
             correspondiente mas cercano"""
-            oracle_near = inst_oracles[n_features:]
-            instance = inst_oracles[:n_features]
-            oracle_near = list(oracle_near)
-            index_classifier = oracle_near.index(1)
+            index_classifier=self._split_inst_oracles(X, inst_oracles)[1]
+            instance=self._split_inst_oracles(X, inst_oracles)[0]
             return self._classifiers_train[index_classifier].predict(
                 [instance])[0]
-
-        n_features = X.shape[1]
         m_oracle = np.concatenate((X, self._nearest_oracle(X)), axis=1)
         self._classifiers_prediction = list(map(list_predict, m_oracle))
         self._classifiers_prediction = np.asarray(self._classifiers_prediction)
@@ -126,12 +155,15 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
         def list_predict_proba(inst_oracles):
             """Predecimos la probabilidad de cada una de las instancias con el
             oraculo correspondiente mas cercano"""
-            oracle_near = inst_oracles[n_features:]
-            instance = inst_oracles[:n_features]
-            oracle_near = list(oracle_near)
-            index_classifier = oracle_near.index(1)
+            index_classifier=self._split_inst_oracles(X, inst_oracles)[1]
+            instance=self._split_inst_oracles(X, inst_oracles)[0]
             prediction_proba = self._classifiers_train[
                 index_classifier].predict_proba([instance])
+            """Calculamos el array con el tamaño mas pequeño, porque podemos
+            tener el problema de que está considerando que la segunda salida
+            solo tiene un valor. En este caso para no tener problemas lo que
+            hacemos es calcular la probabilidad para esa instancia con el
+            predict en vez de usar el predict_proba"""
             if((min(prediction_proba, key=(lambda x: len(x[0])))).shape[1]):
                 call_predict = self._classifiers_train[
                     index_classifier].predict([instance])
@@ -140,7 +172,6 @@ class BaseRandomOracles(ClassifierMixin, BaseEstimator):
             else:
                 return prediction_proba
 
-        n_features = X.shape[1]
         m_oracle = np.concatenate((X, self._nearest_oracle(X)), axis=1)
         self._classifiers_prediction_proba = list(map(
             list_predict_proba, m_oracle))
