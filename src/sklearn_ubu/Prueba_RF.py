6facacba902a4a6f5710cd3b1dff58ba5bb43ec1
Tulmot <etc0007@alu.ubu.es>
2017-12-20 18:09:21 +0100

    Primera versión acabada

--- src/sklearn_ubu/Prueba_RF.py
+++ src/sklearn_ubu/Prueba_RF.py
@@ -12,13 +12,23 @@ seed = 0
 X, y = make_multilabel_classification(
         n_samples=50, n_features=10, random_state=seed)
 
-rf=BaseRotationForest(base_estimator=DecisionTreeClassifier(
-                      random_state=seed),random_state=seed)
-#dn = DisturbingNeighbors(
-#    random_state=seed, base_estimator_=BaseDisturbingNeighbors(
-#            random_state=seed, base_estimator=DecisionTreeClassifier(
-#                random_state=seed)))
+#rf=BaseRotationForest(base_estimator=DecisionTreeClassifier(
+#                      random_state=seed),random_state=seed)
+rf = RotationForest(
+    random_state=seed, base_estimator_=BaseRotationForest(
+            random_state=seed, base_estimator=DecisionTreeClassifier(
+                random_state=seed)))
 X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.5, train_size=0.5, random_state=seed)
 
 clas_train = rf.fit(X_train, y_train)
+
+y_predict = rf.predict(X_test)
+
+y_predict_proba = rf.predict_proba(X_test)
+
+dist = hamming_loss(y_test, y_predict)
+print(dist)
+
+scores = cross_val_score(rf, X, y, cv=3)
+print(scores)
--- src/sklearn_ubu/base_rotation_forest.py
+++ src/sklearn_ubu/base_rotation_forest.py
@@ -17,8 +17,11 @@ class BaseRotationForest(ClassifierMixin, BaseEstimator):
         self.base_estimator = base_estimator
         self.n_groups = n_groups
         self.random_state = random_state
+        self._rnd_features = None
 
-    def split(self, X):
+    def _calc_rnd_features(self, X):
+        """Calculamos un array random para seleccionar unas características
+        aleatorias"""
         tam = X.shape[1]
         list_features = np.arange(tam)
         self.random_state.shuffle(list_features)
@@ -26,9 +29,11 @@ class BaseRotationForest(ClassifierMixin, BaseEstimator):
         while(len(list_features) % self.n_groups != 0):
             random_feature = self.random_state.randint(0, tam)
             list_features.append(random_feature)
-        list_features = np.asarray(list_features)
+        return np.asarray(list_features)
+        
+    def split(self, X):
         divide=np.split(
-            list_features, list_features.shape[0]/self.n_groups)
+            self._rnd_features, self._rnd_features.shape[0]/self.n_groups)
         def separe(rand_features):
             return X[:, rand_features]
         return list(map(separe,divide))
@@ -54,10 +59,56 @@ class BaseRotationForest(ClassifierMixin, BaseEstimator):
             pca.fit(subX)
             return (pca, pca.transform(subX))
         self.random_state = check_random_state(self.random_state)
-        self._split_group = self.split(X)
-        pcas_transform = list(map(pca_transform, self._split_group))
-        tuples_pcas=list(map(pca_transform, self._split_group))
+        self._rnd_features=self._calc_rnd_features(X)
+        split_group = self.split(X)
+        tuples_pcas=list(map(pca_transform, split_group))
         self._pcas = list(map(lambda t: t[0], tuples_pcas))
         pcas_transform = list(map(lambda t: t[1], tuples_pcas))
         pcas_transform = np.concatenate((pcas_transform),axis=1)
-
+        self.base_estimator.fit(pcas_transform,y)
+        
+    def predict(self, X):
+        """Predict class for X.
+        The predicted class of an input sample is computed as the class with
+        the highest mean predicted probability.
+        Parameters
+        ----------
+        X : It's a matrix of form = [n_instances, n_features]
+            The training input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+        Returns
+        -------
+        y : It's a matrix of form = [n_class]
+            The predicted classes.
+        """
+        def pca_transform(pos_subX):
+            return self._pcas[pos_subX[0]].transform(pos_subX[1])
+        split_group = self.split(X)
+        tuple_pos_subX=list(zip(range(len(self._pcas)),split_group))
+        pcas_transform=list(map(pca_transform, tuple_pos_subX))
+        pcas_transform = np.concatenate((pcas_transform),axis=1)
+        return self.base_estimator.predict(pcas_transform)
+    
+    def predict_proba(self, X):
+        """The predicted class probabilities of an input sample is computed as
+        the mean predicted class probabilities of the base estimators in the
+        ensemble.
+         Parameters
+        ----------
+        X : It's a matrix of form = [n_instances, n_features]
+            The training input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+        Returns
+        -------
+        p : It's a matrix of form = [n_samples, n_classes]
+            The class probabilities of the input samples. The order of the
+            classes corresponds to that in the attribute `classes_`.
+        """
+        def pca_transform(pos_subX):
+            return self._pcas[pos_subX[0]].transform(pos_subX[1])
+        split_group = self.split(X)
+        tuple_pos_subX=list(zip(range(len(self._pcas)),split_group))
+        pcas_transform=list(map(pca_transform, tuple_pos_subX))
+        pcas_transform = np.concatenate((pcas_transform),axis=1)
+        return self.base_estimator.predict_proba(pcas_transform)
+        
